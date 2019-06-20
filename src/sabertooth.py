18c7b483f3d67e83431d1c43b8c8b0885fe97f21
Himanshu Garg <himanshu.garg@gmail.com>
2019-06-20 15:27:12 +0530

    updated label display

--- src/sabertooth.py
+++ src/sabertooth.py
@@ -1,3 +1,4 @@
+import pdb
 import tkinter
 import math
 root = tkinter.Tk()
@@ -25,15 +26,9 @@ class line:
 		cosValue=math.cos(math.radians(self.angle))
 		y2=int(self.y1-(sinValue*self.length)) #because it may get into non-terminating, non-reckering decimal values
 		x2=int(self.x1+(cosValue*self.length))
-		textX=(self.x1+x2)/2 #x coordinate for text tag
-		textY=(self.y1+y2)/2+10 #y coordinate for text tag
-		textX=str(textX)
-		textY=str(textY)
-		self.x1=str(self.x1)
-		x2=str(x2)
-		self.y1=str(self.y1)
-		y2=str(y2)
-		file.write('<line x1="'+self.x1+'" y1="'+self.y1+'" x2="'+x2+'" y2="'+y2+'" stroke="'+self.color+'"/>\n')
 		lengthOfMeasurement=len(self.measurement)*8
+		textX=(self.x1+x2-lengthOfMeasurement)/2 #x coordinate for text tag
+		textY=(self.y1+y2+8)/2 #y coordinate for text tag
+		file.write(f'<line x1="{self.x1}" y1="{self.y1}" x2="{x2}" y2="{y2}" stroke="{self.color}"/>\n')
 		lengthOfMeasurement=str(lengthOfMeasurement)
-		file.write('<text x="'+textX+'" y="'+textY+'" fill="'+self.color+'" transform="rotate(-'+str(self.angle)+' 0,0)" lengthAdjust="spacingAndGlyphs" textLength="'+lengthOfMeasurement+'">'+self.measurement+'</text>\n')
+		file.write(f'<text x="0" y="0" fill="{self.color}" transform="translate({textX} {textY}) rotate(-{self.angle} 0,0)" lengthAdjust="spacingAndGlyphs" textLength="{lengthOfMeasurement}">{self.measurement}</text>\n')
--- src/test.py
+++ src/test.py
@@ -1,3 +1,9 @@
 from sabertooth import instigate, line
 instigate()
-l=line(angle=90)
+l=line(angle=15, measurement="15")
+l=line(angle=30, measurement="30")
+l=line(angle=45, measurement="45")
+l=line(angle=60, measurement="60")
+l=line(angle=75, measurement="75")
+l=line(angle=90, measurement="90")
+
