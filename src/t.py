33b2c7cafa49fff5b7d7adc1ab4110c4bd0a56b0
ethereousnatsudragneel <mnjt.singh2000@gmail.com>
2019-07-23 11:59:14 +0530

    to fix the line length bug

--- src/t.py
+++ src/t.py
@@ -1,3 +1,3 @@
-from sabertooth import instigate, line, direction,style
+from sabertooth import instigate, line
 instigate()
 l1=line(length="10cm",label="A,B",extension1={"length":"5cm","direction":"forward","style":"dashed"})
